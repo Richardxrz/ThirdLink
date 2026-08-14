365df00309a38d18b3c94566e63286414c527552
2501_93645483 <2501_93645483@noreply.gitcode.com>
2026-08-14 19:28:50 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Third Link Simulation
+
+## 项目简介
+
+这是一个用于连杆运动学仿真的库
---
+++ toolchains/msys-mingw64-gcc.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME  WINDOWS)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER    E:/Msys/mingw64/bin/gcc.exe)
+set(CMAKE_CXX_COMPILER  E:/Msys/mingw64/bin/g++.exe)
+
+set(CMAKE_AR        E:/Msys/mingw64/bin/ar.exe)
+set(CMAKE_RANLIB    E:/Msys/mingw64/bin/ranlib.exe)
+set(CMAKE_OBJCOPY   E:/Msys/mingw64/bin/objcopy.exe)
+set(CMAKE_OBJDUMP   E:/Msys/mingw64/bin/objdump.exe)
+set(CMAKE_NM        E:/Msys/mingw64/bin/nm.exe)
+set(CMAKE_STRIP     E:/Msys/mingw64/bin/strip.exe)
+set(CMAKE_DLLTOOL   E:/Msys/mingw64/bin/dlltool.exe)
+set(CMAKE_RC_COMPILER E:/Msys/mingw64/bin/windres.exe)
+
+set(CMAKE_FIND_ROOT_PATH    E:/Msys/mingw64)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY  ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE  ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM  NEVER)
+
+set(CMAKE_MAKE_PROGRAM E:/Msys/mingw64/bin/ninja.exe)
